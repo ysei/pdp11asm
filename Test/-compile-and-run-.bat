92e9ab9f4b771c8d59534f0c8c2e1e2a7a96035e
Aleksey <aleksey.f.morozov@gmail.com>
2016-05-11 01:30:22 +0300

    rm

--- Test/-compile-and-run-.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-cls
-pdp11asm.exe bk0010_miner.asm
-if errorlevel 1 goto error
-start C:\emu\EMU.exe bk0010_miner.bin
-exit
-:error
-pause
--- Test/readme.txt
+++
@@ -1,5 +0,0 @@
-Ёмул€тор можно скачать тут
-
-http://bashkiria-2m.narod.ru/index/files/0-11
-
-http://bashkiria-2m.narod.ru/files/emu.rar
